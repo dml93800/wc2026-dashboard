9e0179c794bc2ff184a27c06cc852779fb434dd7
dml93800 <djameljvv@gmail.com>
2026-06-29 08:50:16 +0200

    Add source maps

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  productionBrowserSourceMaps: true,
+  images: {
+    domains: ['flagcdn.com', 'images.pexels.com'],
+  },
 }
-
 module.exports = nextConfig
